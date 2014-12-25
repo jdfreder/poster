66e75a666436a116a73b0bf2bfe207d1da447ebd
Jonathan Frederic <jdfreder@calpoly.edu>
2014-12-24 17:55:04 -0800

    Properly render tab characters

--- source/js/config.js
+++ source/js/config.js
@@ -1,7 +1,13 @@
 // Copyright (c) Jonathan Frederic, see the LICENSE file for more info.
 
 var utils = require('./utils.js');
-exports.config = new utils.PosterClass([
-    'highlight_draw', // bool - Whether or not to highlight re-renders
+var config = new utils.PosterClass([
+    'highlight_draw', // boolean - Whether or not to highlight re-renders
     'newline_width', // integer - Width of newline characters
+    'tab_width', // integer - Tab character width measured in space characters
 ]);
+
+// Set defaults
+config.tab_width = 4;
+
+exports.config = config;
